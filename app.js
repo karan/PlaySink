c7e4975df95f931492043297d0185aaeb70c098f
Sean Aryana <seanaryana@gmail.com>
2013-12-28 10:56:31 -0800

    Attempted to make the login/account setup using the MongoDB. (not working)

--- app.js
+++ app.js
@@ -3,19 +3,30 @@ var app = express();
 var port = 1234;
 var log = console.log;
 
+// Connects to database
+var mongo = require('mongodb');
+var db = require('monk')('localhost/PlaySink');
+
+// All enviroments ?need to figure out what this shit does
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express)
 app.use(express.bodyParser());
 app.use(express.cookieParser());
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 // Homepage render
 app.get('/', function(req, res) {
 	res.render('index');
+	var c = db.usercollection.find();
+	while (c.hasNext) {
+		printjson(c.next());
+	}
+	log('hello');
 });
 
+// User attempts to login checks the details
 app.post('/', function(req, res) {
 	var username = req.body.username;
 	var password = req.body.password;
