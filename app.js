d20a35f03af1eeeee201bfbada0a39121000997a
Karan Goel <karan@goel.im>
2013-12-28 18:24:25 -0800

    sticking with monk for now

--- app.js
+++ app.js
@@ -12,18 +12,19 @@ var db = monk('localhost:27017/playsinkdb'); // default port for mongo, db name
 var app = express(); // create an express app
 
 // all environments
-app.set('port', process.env.PORT || 8888);
-app.set('views', path.join(__dirname, 'views')); // views folder
-app.set('view engine', 'jade'); // using jade
-app.use(express.favicon()); // favicon, can be changed
-app.use(express.logger('dev')); // dev mode
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public'))); // makes dirs look top-level
+app.configure(function(){
+  app.set('port', process.env.PORT || 8888);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('public/javascripts', express.static(path.join(__dirname, 'public/javascripts')));
+  app.use('public/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
+});
 
 // development only
 if ('development' == app.get('env')) {
@@ -34,6 +35,24 @@ app.get('/', routes.index);
 app.get('/userlist', routes.userlist(db)); // pass db to userlist route
 app.get('/newuser', routes.newuser);
 app.post('/adduser', routes.adduser(db)); // POST to db
+
+
+/*===== Error Handlers =====*/
+app.use(function(req, res, next){
+  // the status option, or res.statusCode = 404
+  // are equivalent, however with the option we
+  // get the "status" local available as well
+  res.render('404', {url: req.url});
+});
+
+app.use(function(err, req, res, next){
+  // we may use properties of the error object
+  // here and next(err) appropriately, or if
+  // we possibly recovered from the error, simply next().
+  res.render('500')
+});
+/*===== Error Handlers =====*/
+
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
